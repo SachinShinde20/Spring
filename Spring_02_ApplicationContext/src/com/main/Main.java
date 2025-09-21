f35b76ae8115a1881a964adee467de63a326dbea
SachinShinde20 <shindesachinw@gmail.com>
2025-09-21 22:24:04 +0530

    Update to use ApplicationContext.

---
+++ Spring_02_ApplicationContext/src/com/main/Main.java
@@ -0,0 +1,27 @@
+package com.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+		System.out.println("### applicationContext is created ###");
+
+		System.out.println("\n\t Getting e1");
+		Emp e1 = (Emp) applicationContext.getBean("emp1");
+		System.out.println(e1);
+
+		System.out.println("\n\t Getting e2");
+		Emp e2 = (Emp) applicationContext.getBean("emp2");
+		System.out.println(e2);
+
+		System.out.println("\n\t Getting e3");
+		Emp e3 = (Emp) applicationContext.getBean("emp3");
+		System.out.println(e3);
+
+		System.out.println("\n\t Getting e4");
+		Emp e4 = (Emp) applicationContext.getBean("emp4");
+		System.out.println(e4);
+	}
+}
